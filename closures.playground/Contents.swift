2904e3443576784b2c371285fc7e2a38cb47e5ae
uche-afk <112139467+uche-afk@users.noreply.github.com>
2023-12-19 02:47:30 -0500

    worked on closures

---
+++ closures.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+let mapped = luckyNumbers.filter { !$0.isMultiple(of: 2) }.sorted().map { "\($0) is a lucky number." }
+for phrase in mapped {
+    print(phrase)
+}

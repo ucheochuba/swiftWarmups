d6ab99ca2adc1b59adce2f809e07a12e04dd964d
uche-afk <112139467+ucheochuba@users.noreply.github.com>
2024-02-03 21:52:42 -0800

    Building WeSplit app

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Uche Ochuba on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
